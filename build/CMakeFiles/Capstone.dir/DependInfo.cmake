37d4639e0df04f7b21bd626d716943884cb9eb82
Guilherme Zago <guimzago@gmail.com>
2021-08-14 17:05:53 -0300

    first commit

---
+++ build/CMakeFiles/Capstone.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/guimzago/snake/CppND-Capstone-Snake-Game/src/controller.cpp" "/home/guimzago/snake/CppND-Capstone-Snake-Game/build/CMakeFiles/Capstone.dir/src/controller.cpp.o"
+  "/home/guimzago/snake/CppND-Capstone-Snake-Game/src/game.cpp" "/home/guimzago/snake/CppND-Capstone-Snake-Game/build/CMakeFiles/Capstone.dir/src/game.cpp.o"
+  "/home/guimzago/snake/CppND-Capstone-Snake-Game/src/main.cpp" "/home/guimzago/snake/CppND-Capstone-Snake-Game/build/CMakeFiles/Capstone.dir/src/main.cpp.o"
+  "/home/guimzago/snake/CppND-Capstone-Snake-Game/src/renderer.cpp" "/home/guimzago/snake/CppND-Capstone-Snake-Game/build/CMakeFiles/Capstone.dir/src/renderer.cpp.o"
+  "/home/guimzago/snake/CppND-Capstone-Snake-Game/src/snake.cpp" "/home/guimzago/snake/CppND-Capstone-Snake-Game/build/CMakeFiles/Capstone.dir/src/snake.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Capstone.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Capstone.dir/src/controller.cpp.o"
+  "CMakeFiles/Capstone.dir/src/game.cpp.o"
+  "CMakeFiles/Capstone.dir/src/main.cpp.o"
+  "CMakeFiles/Capstone.dir/src/renderer.cpp.o"
+  "CMakeFiles/Capstone.dir/src/snake.cpp.o"
+  "Capstone"
+  "Capstone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Capstone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
